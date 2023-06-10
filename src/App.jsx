50fb5ea21b720737ff5e9b2f3f3e9f71cea9aca4
Segnicho <segnidesta60@gmail.com>
2023-06-10 20:47:39 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LeftSideBar from './components/LeftSideBar';
+import Navbar from "./components/Navbar";
+function App() {
+
+  return (
+    <div>
+      <LeftSideBar />
+      <h1 className="">Hello</h1>
+    </div>
+  );
+}
+
+export default App
